308bf32dd0435fdda3fdb2b00d73ad661df1ae2f
wjdgusdh2887 <wjdgusdh2887@sparrowfasoo.com>
2021-05-04 09:08:51 +0900

    refactor : change constant number to wrapper and remove unused import

--- src/test/java/util/NumberUtilTest.java
+++ src/test/java/util/NumberUtilTest.java
@@ -1,7 +1,6 @@
 package util;
 
 import checker.ListChecker;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -10,12 +9,15 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class NumberUtilTest {
+    private static final int LOWER_BOUND = 0;
+    private static final int UPPER_BOUND = 9;
+
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
     public void makeRandomNumberList(int input) {
-        List<Integer> randomList = NumberUtil.makeRandomNumberList(input, 0, 9);
+        List<Integer> randomList = NumberUtil.makeRandomNumberList(input, LOWER_BOUND, UPPER_BOUND);
         assertThat(randomList)
                 .hasSize(input);
-        ListChecker.checkListItemRange(randomList, 0, 9);
+        ListChecker.checkListItemRange(randomList, LOWER_BOUND, UPPER_BOUND);
     }
 }
