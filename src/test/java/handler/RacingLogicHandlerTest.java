99a02aa2745f70ce950c7fe7d6a06bda89c91617
wjdgusdh2887 <wjdgusdh2887@sparrowfasoo.com>
2021-05-04 09:42:52 +0900

    refactor : change test class name for racing logic handler

--- src/test/java/handler/RacingLogicHandlerTest.java
+++ src/test/java/handler/RacingLogicHandlerTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class RacintLogicHandlerTest {
+public class RacingLogicHandlerTest {
     @ParameterizedTest
     @ValueSource(strings = {"failcar1,car1,failcar2,car3"})
     public void makeCarByInput_2success(String input) {
