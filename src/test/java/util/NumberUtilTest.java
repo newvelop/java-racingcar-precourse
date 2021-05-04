9d4f7f3d1f6287f3a33abf74c89466718c7e5f8d
wjdgusdh2887 <wjdgusdh2887@sparrowfasoo.com>
2021-05-04 09:01:54 +0900

    test : remove repeated test annotation for proper test

--- src/test/java/util/NumberUtilTest.java
+++ src/test/java/util/NumberUtilTest.java
@@ -2,6 +2,7 @@ package util;
 
 import checker.ListChecker;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
@@ -9,12 +10,12 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class NumberUtilTest {
-    @RepeatedTest(10)
-    @ValueSource(ints = {1, 2, 3})
-    public void makeRandomNumberList(int size) {
-        List<Integer> randomList = NumberUtil.makeRandomNumberList(size, 0, 9);
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+    public void makeRandomNumberList(int input) {
+        List<Integer> randomList = NumberUtil.makeRandomNumberList(input, 0, 9);
         assertThat(randomList)
-                .hasSize(size);
+                .hasSize(input);
         ListChecker.checkListItemRange(randomList, 0, 9);
     }
 }
